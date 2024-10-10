ba32ab69acc3a0b8d8ecef637f107e4dd7999566
HenkSeed <adaner@inbox.ru>
2024-10-10 23:03:51 +0700

    Change colors of mobile header and its icons, add mobile modules into every page
    Tabs gap, gap footer phones, shorten copyright message, add class =text=, made padding 20px for small screens

---
+++ dist/js/modules/card_product_slider_big.js
@@ -0,0 +1,57 @@
+const productSlider = new Swiper(".product-slider__main-swiper", {
+    loop: true,
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: ".product-slider__arrow-left",
+    //     prevEl: ".product-slider__arrow-right",
+    // },
+    slideToClickedSlide: true,
+
+    autoHeight: false,
+    // slidesPerView: "auto",
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    // watchOverflow: true,
+    // breakpoints: {
+    //     320: {
+    //         slidesPerView: 2,
+    //         // spaceBetween: 10,
+    //     },
+    //     480: {
+    //         slidesPerView: 3,
+    //         // spaceBetween: 15,
+    //     },
+    // },
+    speed: 600,
+    thumbs: {
+        swiper: {
+            el: ".product-slider__swiper",
+            loop: true,
+
+            // Navigation arrows
+            navigation: {
+                nextEl: ".product-slider__arrow-left",
+                prevEl: ".product-slider__arrow-right",
+            },
+            slideToClickedSlide: true,
+
+            // autoHeight: true,
+            // slidesPerView: "auto",
+            slidesPerView: 3,
+            spaceBetween: 30,
+            watchOverflow: true,
+            breakpoints: {
+                320: {
+                    slidesPerView: 2,
+                    // spaceBetween: 10,
+                },
+                480: {
+                    slidesPerView: 3,
+                    // spaceBetween: 15,
+                },
+            },
+            speed: 600,
+        },
+    },
+});
